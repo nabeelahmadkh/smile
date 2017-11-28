42e33f310806efd7f94626095168dcbedc532463
Nabeel Ahmad <nabeelahmadkh@gmail.com>
2017-11-27 19:29:32 -0600

    fixed meme(instagram) issue

--- smile/PostTableViewCell.swift
+++ smile/PostTableViewCell.swift
@@ -16,7 +16,7 @@ class PostTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        print("awkake from nib called ")
+        print("awake from nib called ")
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {

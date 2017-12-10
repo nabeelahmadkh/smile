9dabe86991886642bdbdb60559641c470b2ec238
Nabeel Ahmad <nabeelahmadkh@gmail.com>
2017-12-10 16:31:55 -0600

    add game and like functionality in memes

--- smile/controllers/PhoneViewController.swift
+++ smile/controllers/PhoneViewController.swift
@@ -13,8 +13,27 @@ class PhoneViewController: UIViewController{
     
     @IBOutlet weak var textLabel: UILabel!
     
-    @IBAction func callHelpline(_ sender: Any) {
+    @IBAction func callHelpline2(_ sender: Any) {
         let number = "+13194008304"
+        
+        let formattedNumber = number.components(separatedBy:
+            NSCharacterSet.decimalDigits.inverted).joined(separator: "")
+        
+        let phoneUrl = "tel://\(formattedNumber)"
+        let url:NSURL = NSURL(string: phoneUrl)!
+        print("URLS IS \(url)")
+        
+        if #available(iOS 10, *) {
+            print("ENTERED IF CASE")
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+        } else {
+            print("ENTERED ELSE CASE")
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func callHelpline(_ sender: Any) {
+        let number = "+18002738255"
         
         let formattedNumber = number.components(separatedBy:
             NSCharacterSet.decimalDigits.inverted).joined(separator: "")
@@ -35,6 +54,6 @@ class PhoneViewController: UIViewController{
     
     override func viewDidLoad() {
         textLabel = ComponentFormatter().setLabel(textLabel, 18, UIColor.black)
-        self.title = "HelpLine"
+        self.title = "HelpLines"
     }
 }
